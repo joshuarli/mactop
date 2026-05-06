c90feac4cb9345f84a438ca81041c1b231b74677
Joshua Li <joshuarli98@gmail.com>
2026-05-06 08:35:27 -0700

    swiftlint is trash

--- Package.swift
+++ Package.swift
@@ -4,18 +4,12 @@ import PackageDescription
 let package = Package(
     name: "mactop",
     platforms: [.macOS(.v15)],
-    dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.57.0"),
-    ],
     targets: [
         .executableTarget(
             name: "mactop",
             swiftSettings: [.swiftLanguageMode(.v5)],
             linkerSettings: [
                 .linkedFramework("IOKit"),
-            ],
-            plugins: [
-                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint"),
             ]
         )
     ]
